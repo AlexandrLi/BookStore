6a5ceb4525db0a6cf8148f77ea86f4dbe8c5e62c
Alexandr_Li <muzikant1990@gmail.com>
2016-07-25 11:52:17 +0600

    Add Customer.class

---
+++ src/main/java/com/epam/ali/javaee7/model/Customer.java
@@ -0,0 +1,93 @@
+package com.epam.ali.javaee7.model;
+
+import com.epam.ali.javaee7.annotation.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class Customer {
+    @NotNull
+    @Size(min = 2)
+    private String firstName;
+    private String lastName;
+    @Email
+    private String email;
+    private String phoneNumber;
+    @Past
+    private Date dateOfBirth;
+    private Address deliveryAddress;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String email, String phoneNumber, Date dateOfBirth, Address deliveryAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Address getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(Address deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", deliveryAddress=" + deliveryAddress +
+                '}';
+    }
+}
