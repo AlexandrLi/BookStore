bd2a308c9f65307f92afcae3bb7bf88e46cac50b
Alexandr_Li <muzikant1990@gmail.com>
2016-07-26 17:43:50 +0600

    Add persistBook() method

--- src/main/java/com/epam/ali/javaee7/service/BookService.java
+++ src/main/java/com/epam/ali/javaee7/service/BookService.java
@@ -1,11 +1,15 @@
 package com.epam.ali.javaee7.service;
 
-import com.epam.ali.javaee7.util.NumberGenerator;
 import com.epam.ali.javaee7.annotation.Loggable;
 import com.epam.ali.javaee7.annotation.ThirteenDigits;
 import com.epam.ali.javaee7.model.Book;
+import com.epam.ali.javaee7.util.NumberGenerator;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 @Loggable
 public class BookService {
@@ -13,9 +17,19 @@ public class BookService {
     @ThirteenDigits
     private NumberGenerator numberGenerator;
 
-    public Book createBook(String title, Float price, String description) {
-        Book book = new Book(title, price, description);
-        book.setNumber(numberGenerator.generateNumber());
-        return book;
+    public Book createBook(String title, Float price, String description, Integer numberOfPages, Boolean illustrations) {
+        String isbn = numberGenerator.generateNumber();
+        return new Book(title, price, description, isbn, numberOfPages, illustrations);
+    }
+
+    public void persistBook(Book book) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bookStore");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(book);
+        transaction.commit();
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
