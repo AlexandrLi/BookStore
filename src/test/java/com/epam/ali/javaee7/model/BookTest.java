e685a324c5ec72b42d5194aa48f2096a2e9e9c1e
Alexandr_Li <muzikant1990@gmail.com>
2016-08-02 12:30:55 +0600

    Code refactoring in BookTest.class

--- src/test/java/com/epam/ali/javaee7/model/BookTest.java
+++ src/test/java/com/epam/ali/javaee7/model/BookTest.java
@@ -5,54 +5,52 @@ import junit.framework.TestCase;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import javax.validation.ConstraintViolationException;
 
 public class BookTest extends TestCase {
 
-    private EntityManager entityManager;
-    private EntityTransaction transaction;
+    private EntityManager em;
+    private EntityTransaction tx;
     private BookService bookService;
     private WeldContainer container;
 
     public void setUp() {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bookStoreTest");
-        entityManager = entityManagerFactory.createEntityManager();
-        transaction = entityManager.getTransaction();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bookStoreTest");
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
         Weld weld = new Weld();
         container = weld.initialize();
         bookService = container.instance().select(BookService.class).get();
     }
 
     public void tearDown() {
-        if (entityManager != null) {
-            entityManager.close();
+        if (em != null) {
+            em.close();
             container.close();
         }
     }
 
     public void testShouldFindJavaee7Book() {
-        Book book = entityManager.find(Book.class, 1001L);
+        Book book = em.find(Book.class, 1001L);
         assertEquals("Learn Java EE 7", book.getTitle());
     }
 
     public void testShouldCreateJavaBook() {
         Book book = bookService.createBook("Java", 12.5f, "Java EE 7 book", 760, true);
-        transaction.begin();
-        entityManager.persist(book);
-        transaction.commit();
+        tx.begin();
+        em.persist(book);
+        tx.commit();
         assertNotNull("Id can not be empty", book.getId());
-        book = entityManager.createNamedQuery("findBookJava", Book.class).getSingleResult();
-        assertEquals("Java EE 7 book", book.getDescription());
+        TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b WHERE b.id=" + book.getId(), Book.class);
+        Book result = query.getSingleResult();
+        assertEquals("Java EE 7 book", result.getDescription());
     }
 
     public void testShouldRaiseConstraintViolationCauseNullTitle() {
         try {
             Book book = bookService.createBook(null, 12.5f, "Empty", 760, true);
-            entityManager.persist(book);
+            em.persist(book);
             fail("Test failed");
         } catch (ConstraintViolationException e) {
             assertTrue("Test passed", true);
