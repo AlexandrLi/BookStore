9d2f839e17f6a756f7d6a54bed11d4f0db4edd15
Alexandr_Li <muzikant1990@gmail.com>
2016-08-02 12:33:06 +0600

    Add getAllBooks() and getBooksByTitle() methods

--- src/main/java/com/epam/ali/javaee7/service/BookService.java
+++ src/main/java/com/epam/ali/javaee7/service/BookService.java
@@ -3,13 +3,15 @@ package com.epam.ali.javaee7.service;
 import com.epam.ali.javaee7.annotation.Loggable;
 import com.epam.ali.javaee7.annotation.ThirteenDigits;
 import com.epam.ali.javaee7.model.Book;
+import com.epam.ali.javaee7.model.Book_;
 import com.epam.ali.javaee7.util.NumberGenerator;
 
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Loggable
 public class BookService {
@@ -32,5 +34,28 @@ public class BookService {
         entityManager.close();
         entityManagerFactory.close();
         return book;
+    }
+
+    public List<Book> getAllBooks() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bookStore");
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<Book> query = em.createNamedQuery("Book.findAll", Book.class);
+        List<Book> books = query.getResultList();
+        em.close();
+        emf.close();
+        return books;
+    }
+
+    public List<Book> getBooksByTitle(String title) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bookStore");
+        EntityManager em = emf.createEntityManager();
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Book> query = builder.createQuery(Book.class);
+        Root<Book> book = query.from(Book.class);
+        query.select(book).where(builder.equal(book.get(Book_.title), title));
+        List<Book> books = em.createQuery(query).getResultList();
+        em.close();
+        emf.close();
+        return books;
     }
 }
