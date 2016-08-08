8f2db32e320abf7735c7af8de30a9640a0aa6b97
Alexandr_Li <muzikant1990@gmail.com>
2016-08-08 15:32:03 +0600

    Refactor BookTest

--- src/test/java/com/epam/ali/javaee7/model/BookTest.java
+++ src/test/java/com/epam/ali/javaee7/model/BookTest.java
@@ -1,9 +1,6 @@
 package com.epam.ali.javaee7.model;
 
-import com.epam.ali.javaee7.service.BookService;
 import junit.framework.TestCase;
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
 
 import javax.persistence.*;
 import javax.validation.ConstraintViolationException;
@@ -12,22 +9,16 @@ public class BookTest extends TestCase {
 
     private EntityManager em;
     private EntityTransaction tx;
-    private BookService bookService;
-    private WeldContainer container;
 
     public void setUp() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("bookStoreTest");
         em = emf.createEntityManager();
         tx = em.getTransaction();
-        Weld weld = new Weld();
-        container = weld.initialize();
-        bookService = container.instance().select(BookService.class).get();
     }
 
     public void tearDown() {
         if (em != null) {
             em.close();
-            container.close();
         }
     }
 
@@ -37,17 +28,17 @@ public class BookTest extends TestCase {
     }
 
     public void testShouldCreateJavaBook() {
-        Book book = bookService.createBook("Java", 12.5f, "Java EE 7 book", 760, true);
+        Book book = new Book("Java", 12.5f, "Java EE 7 book", "13-84356-8", 760, true);
         tx.begin();
         em.persist(book);
         tx.commit();
         assertNotNull("Id can not be empty", book.getId());
-        assertEquals(new Integer(1),book.getVersion());
+        assertEquals(new Integer(1), book.getVersion());
         tx.begin();
         book = em.find(Book.class, book.getId());
         book.setDescription("Java EE 7 book version 2");
         tx.commit();
-        assertEquals(new Integer(2),book.getVersion());
+        assertEquals(new Integer(2), book.getVersion());
         TypedQuery<Book> query = em.createQuery("SELECT b FROM Book b WHERE b.id=" + book.getId(), Book.class);
         Book result = query.getSingleResult();
         assertEquals("Java EE 7 book version 2", result.getDescription());
@@ -55,7 +46,7 @@ public class BookTest extends TestCase {
 
     public void testShouldRaiseConstraintViolationCauseNullTitle() {
         try {
-            Book book = bookService.createBook(null, 12.5f, "Empty", 760, true);
+            Book book = new Book(null, 12.5f, "Empty", "13-84356-8", 760, true);
             em.persist(book);
             fail("Test failed");
         } catch (ConstraintViolationException e) {
