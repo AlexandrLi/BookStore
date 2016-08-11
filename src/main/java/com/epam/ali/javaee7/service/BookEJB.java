0de19f9d6644d23981ab6d864de5f9906fa1d763
Alexandr_Li <muzikant1990@gmail.com>
2016-08-11 10:53:45 +0600

    Refactor BookEJB

--- src/main/java/com/epam/ali/javaee7/service/BookEJB.java
+++ src/main/java/com/epam/ali/javaee7/service/BookEJB.java
@@ -1,78 +1,45 @@
 package com.epam.ali.javaee7.service;
 
-import com.epam.ali.javaee7.annotation.Loggable;
-import com.epam.ali.javaee7.annotation.ThirteenDigits;
 import com.epam.ali.javaee7.model.Book;
-import com.epam.ali.javaee7.model.Book_;
-import com.epam.ali.javaee7.util.NumberGenerator;
 
-import javax.annotation.Resource;
-import javax.ejb.EJB;
-import javax.ejb.SessionContext;
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Loggable
 @Stateless
-public class BookEJB {
-
-    private static final int INVENTORY_MAX_CAPACITY = 1000;
-
-    @PersistenceContext(unitName = "bookStore")
-    private EntityManager em;
-
-    @EJB
-    private CustomerEJB customerEJB;
+@LocalBean
+public class BookEJB implements BookEJBRemote {
 
     @Inject
-    @ThirteenDigits
-    private NumberGenerator numberGenerator;
+    private EntityManager em;
 
-    @Resource
-    private SessionContext context;
-
-    @Resource(name = "currencyEntry")
-    private Float changeRate;
-    @Resource(name = "changeRateEntry")
-    private String currency;
-
-    public Book persistBook(Book book) {
-        if (!context.isCallerInRole("admin")) {
-            throw new SecurityException("Only administrators can create books");
-        }
-        em.persist(book);
-        if (inventoryLevel(book) > INVENTORY_MAX_CAPACITY) {
-            context.setRollbackOnly();
-        }
-        return book;
-    }
-
-    private int inventoryLevel(Book book) {
-        // TODO: 8/8/2016 Mock method. Add business logic
-        return 0;
-    }
-
-    public List<Book> getAllBooks() {
+    @Override
+    public List<Book> findBooks() {
         TypedQuery<Book> query = em.createNamedQuery(Book.FIND_ALL, Book.class);
         return query.getResultList();
     }
 
-    public List<Book> getBooksByTitle(String title) {
-        CriteriaBuilder builder = em.getCriteriaBuilder();
-        CriteriaQuery<Book> query = builder.createQuery(Book.class);
-        Root<Book> book = query.from(Book.class);
-        query.select(book).where(builder.equal(book.get(Book_.title), title));
-        return em.createQuery(query).getResultList();
+    @Override
+    public
+    @NotNull
+    Book createBook(@NotNull Book book) {
+        em.persist(book);
+        return book;
     }
 
-    public Book convertPrice(Book book) {
-        book.setPrice(book.getPrice() * changeRate);
-        book.setCurrency(currency);
-        return book;
+    @Override
+    public
+    @NotNull
+    Book updateBook(@NotNull Book book) {
+        return em.merge(book);
+    }
+
+    @Override
+    public void deleteBook(@NotNull Book book) {
+        em.remove(em.merge(book));
     }
 }
