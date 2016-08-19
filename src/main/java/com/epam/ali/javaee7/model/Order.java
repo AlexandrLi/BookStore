5c3f80169b4a60447e725a763fa6e6d86e0bdaed
Alexandr_Li <muzikant1990@gmail.com>
2016-08-19 17:44:01 +0600

    Remove unused classes

--- src/main/java/com/epam/ali/javaee7/model/Order.java
+++
@@ -1,5 +0,0 @@
-package com.epam.ali.javaee7.model;
-
-public class Order {
-    // TODO: 8/9/2016 Add Entity realization
-}
--- src/main/java/com/epam/ali/javaee7/util/NumberGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.epam.ali.javaee7.util;
-
-public interface NumberGenerator {
-
-    String generateNumber();
-}
